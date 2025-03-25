da4edea76188f55a988091c86159aa7b3d495e46
Abraham Mitchell <abemitch06500@hotmail.com>
2025-03-25 15:42:38 -0400

    Initial Commit

---
+++ PhotoSafe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoSafe
+//
+//  Created by Abraham Mitchell on 3/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoSafe/PhotoSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSafeApp.swift
+//  PhotoSafe
+//
+//  Created by Abraham Mitchell on 3/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
