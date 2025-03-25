6af8b1150cd1bede0b76c0bff75bf613aedf6b76
Abraham Mitchell <abemitch06500@hotmail.com>
2025-03-25 16:11:26 -0400

    Created basic header

--- PhotoSafe/ContentView.swift
+++ PhotoSafe/ContentView.swift
@@ -7,6 +7,53 @@
 
 import SwiftUI
 
+struct Header: View {
+    var body: some View {
+        VStack(spacing:0) {
+            HStack {
+                Button {
+                   print("Hi")
+                } label: {
+                    Image(systemName:"trash.fill")
+                        .font(.title2)
+                        .foregroundColor(.red)
+                }
+
+                
+                Spacer()
+                
+                Menu {
+                    Button {
+                        print("Create")
+                    } label: {
+                        Label {
+                            Text("New Album")
+                        } icon: {
+                            Image(systemName: "plus")
+                        }
+                    }
+
+                } label: {
+                    Image(systemName: "gear")
+                        .font(.title2)
+                        .foregroundColor(.primary)
+                }
+            }
+            .frame(height:24)
+            .padding(.horizontal)
+            .padding(.vertical,10)
+        }
+        #if os(iOS)
+            .background(.bar)
+        #endif
+        .overlay(
+            Text("Albums")
+                .font(.title3.bold())
+                .foregroundColor(.primary)
+        )
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
