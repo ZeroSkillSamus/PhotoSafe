b090acd754bcd6f90e428eaeb00abf4a1bd150b9
Abraham Mitchell <abemitch06500@hotmail.com>
2025-03-26 10:25:28 -0400

    Created new file for LockScreen

--- PhotoSafe/LockScreen.swift
+++ PhotoSafe/LockScreen.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct LockScreen: View {
+    @State private var numberInput: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // Title Of Page
+            Text("Enter Password")
+                .font(.title)
+                .fontWeight(.semibold)
+            
+            // Create a Custom NumPad
+        }
+        .frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .top)
     }
 }
 
 #Preview {
-    LockScreen()
+    LockScreen().preferredColorScheme(.dark)
 }
