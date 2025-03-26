58725f89be3489ce4b04b28f7accbe5ad9a697b4
Abraham Mitchell <abemitch06500@hotmail.com>
2025-03-26 10:25:11 -0400

    Finished creating basic layout for albums page

--- PhotoSafe/ContentView.swift
+++ PhotoSafe/ContentView.swift
@@ -11,17 +11,20 @@ struct Header: View {
     var body: some View {
         VStack(spacing:0) {
             HStack {
+                // Display Alert Confirming With User
+                // If they Want Albums to be removed
                 Button {
-                   print("Hi")
+                   print("Delete All")
                 } label: {
                     Image(systemName:"trash.fill")
                         .font(.title2)
                         .foregroundColor(.red)
                 }
-
                 
                 Spacer()
                 
+                // Created a menu for now
+                // 1) Allow User To Create An Album
                 Menu {
                     Button {
                         print("Create")
@@ -31,6 +34,16 @@ struct Header: View {
                         } icon: {
                             Image(systemName: "plus")
                         } 
+                    }
+                    
+                    Button {
+                        print("Edit")
+                    } label: {
+                        Label {
+                            Text("Edit")
+                        } icon: {
+                            Image(systemName: "pencil")
+                        }
                     }
 
                 } label: {
@@ -54,14 +67,64 @@ struct Header: View {
     }
 }
 
+// Create Dummy Data
+struct Album: Hashable {
+    var is_locked: Bool
+    var name: String
+    var image: String
+}
+
+var dummydata = [
+    Album(is_locked: true, name: "TEST1", image: ""),
+    Album(is_locked: true, name: "TEST2", image: ""),
+    Album(is_locked: false, name: "TEST3", image: ""),
+    Album(is_locked: false, name: "TEST4", image: "")
+]
+
+
 struct ContentView: View {
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             Header()
-            Spacer()
+
+            ScrollView {
+                LazyVStack(spacing: 0) {
+                    ForEach(dummydata, id:\.self) { album in
+                        HStack {
+                            AsyncImage(url: URL(string: "https://picsum.photos/id/237/400/400")) { image in
+                                image
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                            } placeholder: {
+                                Color.gray
+                            }
+                            .frame(width: 80, height: 95)
+   
+                            HStack {
+                                Text(album.name)
+                                    .padding(.leading,20)
+                                
+                                Spacer()
+                                
+                                if album.is_locked {
+                                    Image(systemName: "lock.fill")
+                                        .font(.title2)
+                                        .foregroundColor(.primary)
+                                        .padding(.trailing,20)
+                                }
+                            }
+                            .frame(maxWidth:.infinity)
+                            
+                            
+                        }
+                        Divider()
+                    }
+                }
+            }
             
+            // Ensure Everything Starts At Top of Page 
+            Spacer()
         }
-        //.padding()
     }
 }
 
---
+++ PhotoSafe/LockScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LockScreen.swift
+//  PhotoSafe
+//
+//  Created by Abraham Mitchell on 3/26/25.
+//
+
+import SwiftUI
+
+struct LockScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LockScreen()
+}
