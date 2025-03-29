12e456d4be3a55624515a9132a13abf6c6ba37f9
Abraham Mitchell <abemitch06500@hotmail.com>
2025-03-28 22:03:06 -0400

    Cleaned up album page,   Included password for albums   Display alert before deleting all albums   Changed Navigation Stack with Path

--- PhotoSafe/ContentView.swift
+++ PhotoSafe/ContentView.swift
@@ -7,6 +7,11 @@
 import PhotosUI
 import SwiftUI
 
+enum KindOfTextBox {
+    case Password
+    case Name
+}
+
 struct CreateAlbum: View {
     @Environment(\.dismiss) private var dismiss
     
@@ -14,8 +19,47 @@ struct CreateAlbum: View {
     @State private var avatar_data: Data?
     @State private var name: String = ""
     @State private var is_locked: Bool = false
+    @State private var password: String = ""
     
     @ObservedObject var album_vm: AlbumViewModel
+    
+    func determine_if_button_disabled() -> Bool {
+        if self.name.isEmpty || (self.is_locked && self.password.isEmpty) {
+            return true
+        }
+        
+        return false
+    }
+    
+    private struct TextBox: View {
+        var header: String
+        var type: KindOfTextBox
+        
+        @Binding var text: String
+        
+        var body: some View {
+            VStack(spacing: 5) {
+                Text(header)
+                    .font(.system(size: 18,weight: .semibold,design: .rounded))
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                
+                TextField("", text: self.$text)
+                    .fontWeight(.semibold)
+                    .textInputAutocapitalization(type == .Name ? .words : .never)
+                    .autocorrectionDisabled()
+                    .padding(EdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 0))
+                    .foregroundStyle(.black)
+                    .background(Color.primary)
+                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                    .onChange(of: self.text) {
+                        // Check to see if its in character limit (15)
+                        if text.count > 15 {
+                            text.removeLast()
+                        }
+                    }
+            }
+        }
+    }
     
     var body: some View {
         VStack {
@@ -45,44 +89,31 @@ struct CreateAlbum: View {
                     .frame(maxHeight: .infinity,alignment: .top)
                 
                 VStack {
-                    VStack(spacing: 5) {
-                        Text("Enter Album Name")
-                            .autocorrectionDisabled()
-                            .font(.system(size: 15,weight: .semibold,design: .rounded))
-                            .frame(maxWidth: .infinity,alignment: .leading)
-                        
-                        TextField("", text: $name)
-                            .fontWeight(.semibold)
-                            .padding(EdgeInsets(top: 0, leading: 2, bottom: 0, trailing: 0))
-                            .foregroundStyle(.black)
-                            .background(Color.primary)
-                            .clipShape(RoundedRectangle(cornerRadius: 5))
-                            .onChange(of: self.name) {
-                                // Check to see if its in character limit (10)
-                                if name.count > 10 {
-                                    name.removeLast()
-                                }
-                            }
+                    TextBox(header: "Name", type: .Name, text: self.$name)
+                    
+                    if self.is_locked {
+                        TextBox(header: "Password", type: .Password, text: self.$password)
                     }
                     
                     Toggle(isOn: self.$is_locked) {
                         Text("Make Private")
                             .font(.system(size: 18,weight: .semibold,design: .rounded))
                     }
-                    
+          
                     Button {
                         self.album_vm.create_album(
                             name: self.name,
                             image_data: self.avatar_data,
-                            is_locked: self.is_locked
+                            is_locked: self.is_locked,
+                            password: self.password
                         )
                         self.dismiss() // Close Sheet
                     } label: {
                         Text("Create")
                             .font(.title3)
                             .fontWeight(.bold)
-                            .disabled(self.name.isEmpty)
                     }
+                    .disabled(self.determine_if_button_disabled())
                     .buttonStyle(.borderedProminent)
                     .tint(.green)
                     .buttonBorderShape(.roundedRectangle(radius: 10))
@@ -111,6 +142,7 @@ struct Header: View {
     @ObservedObject var album_vm: AlbumViewModel
     
     @State private var display_sheet: Bool = false
+    @State private var display_delete_alert: Bool = false
     
     var body: some View {
         VStack(spacing:0) {
@@ -118,7 +150,7 @@ struct Header: View {
                 // Display Alert Confirming With User
                 // If they Want Albums to be removed
                 Button {
-                    self.album_vm.delete_all_albums()
+                    self.display_delete_alert = true
                 } label: {
                     Image(systemName:"trash.fill")
                         .font(.title2)
@@ -163,64 +195,115 @@ struct Header: View {
         .background(.bar)
         .overlay(
             Text("Albums")
-                .font(.title3.bold())
+                .font(.title2.bold())
                 .foregroundColor(.primary)
         )
         .sheet(isPresented: self.$display_sheet) {
             CreateAlbum(album_vm: self.album_vm)
+        }
+        .alert("Delete All Albums?", isPresented: $display_delete_alert) {
+            Button("Delete", role: .destructive) {
+                withAnimation(.easeInOut) {
+                    self.album_vm.delete_all_albums()
+                }
+            }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Will Delete All Albums & Media, Are you Sure????")
         }
     }
 }
 
 struct ContentView: View {
     @StateObject private var album_VM = AlbumViewModel()
-    
+    @State private var display_alert: Bool = false
+    @State private var path = NavigationPath()  // For NavigationStack
+    @State private var password: String = ""
+  
     var body: some View {
-        VStack(spacing: 0) {
-            Header(album_vm: self.album_VM)
-
-            ScrollView {
-                LazyVStack(spacing: 0) {
-                    ForEach(self.album_VM.albums, id:\.self) { album in
-                        HStack {
-                            if let data = album.image, let ui_image = UIImage(data: data) {
-                                Image(uiImage: ui_image)
-                                    .resizable()
-                                    .frame(width: 100, height: 125)
-                                
+        NavigationStack(path: $path) {
+            VStack(spacing: 0) {
+                Header(album_vm: self.album_VM)
+                
+                ScrollView {
+                    LazyVStack(spacing: 0) {
+                        ForEach(self.album_VM.albums, id:\.self) { album in
+                            if album.is_locked {
+                                Button {
+                                    self.display_alert = true
+                                } label: {
+                                    AlbumDisplay(album: album)
+                                }
+                                .alert("Enter Password For \(album.name!)",
+                                       isPresented: self.$display_alert)
+                                {
+                                    TextField("Enter Your Password", text: self.$password)
+                                        .foregroundStyle(.white)
+                                        .autocorrectionDisabled()
+                                        .textInputAutocapitalization(.never)
+                                    
+                                    Button("OK", role: .cancel) {
+                                        if let album_password = album.password,
+                                            album_password == password {
+                                            path.append(album)
+                                        }
+                                    }
+                                }
                             } else {
-                                Image("NoImageFound")
-                                    .resizable()
-                                    .frame(width: 100, height: 125)
-                            }
-                            
-                            HStack {
-                                Text(album.name!)
-                                    .padding(.leading,20)
-                                    .font(.system(size: 18,weight: .medium,design: .rounded))
-                                
-                                Spacer()
-                                
-                                if album.is_locked {
-                                    Image(systemName: "lock.fill")
-                                        .font(.title2)
-                                        .foregroundColor(.primary)
-                                        .padding(.trailing,20)
+                                NavigationLink(value: album) {
+                                    AlbumDisplay(album: album)
                                 }
                             }
-                            .frame(maxWidth:.infinity)
-                            
-                            
+                            Divider()
                         }
-                        Divider()
                     }
                 }
+                
+                // Ensure Everything Starts At Top of Page
+                Spacer()
             }
-            
-            // Ensure Everything Starts At Top of Page
-            Spacer()
+            .navigationDestination(for: AlbumEntity.self) { album in
+                MediaDisplay(name: album.name!)
+            }
         }
     }
+    
+    struct AlbumDisplay: View {
+        var album: AlbumEntity
+
+        var body: some View {
+            HStack {
+                if let data = album.image, let ui_image = UIImage(data: data) {
+                    Image(uiImage: ui_image)
+                        .resizable()
+                        .frame(width: 100, height: 125)
+                    
+                } else {
+                    Image("NoImageFound")
+                        .resizable()
+                        .frame(width: 100, height: 125)
+                }
+                
+                HStack {
+                    Text(album.name!)
+                        .foregroundStyle(.white)
+                        .padding(.leading,20)
+                        .font(.system(size: 18,weight: .medium,design: .rounded))
+                    
+                    Spacer()
+                    
+                    if album.is_locked {
+                        Image(systemName: "lock.fill")
+                            .font(.title2)
+                            .foregroundColor(.primary)
+                            .padding(.trailing,20)
+                    }
+                }
+                .frame(maxWidth:.infinity)
+            }
+        }
+    }
+    
 }
 
 #Preview {
---
+++ PhotoSafe/MediaDisplay.swift
@@ -0,0 +1,18 @@
+//
+//  MediaDisplay.swift
+//  PhotoSafe
+//
+//  Created by Abraham Mitchell on 3/28/25.
+//
+
+import SwiftUI
+
+struct MediaDisplay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MediaDisplay()
+}
