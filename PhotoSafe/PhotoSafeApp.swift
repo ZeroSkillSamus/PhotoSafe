3f69847b7d36252779d8e3d79a43b3fa6de5d4b9
Abraham Mitchell <abemitch06500@hotmail.com>
2025-03-31 15:32:01 -0400

    Changed screens for testing

--- PhotoSafe/PhotoSafeApp.swift
+++ PhotoSafe/PhotoSafeApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct PhotoSafeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //LockScreen().preferredColorScheme(.dark)
+            ContentView().preferredColorScheme(.dark)
         }
     }
 }
