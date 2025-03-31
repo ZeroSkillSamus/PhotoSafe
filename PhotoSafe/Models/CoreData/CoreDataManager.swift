ace29d9499fc872c87bf11c20f987c8ed205d437
Abraham Mitchell <abemitch06500@hotmail.com>
2025-03-31 16:19:16 -0400

    Created CoreDataManager that will manage all containers in app

--- PhotoSafe/Models/CoreData/CoreDataManager.swift
+++ PhotoSafe/Models/CoreData/CoreDataManager.swift
@@ -5,4 +5,18 @@
 //  Created by Abraham Mitchell on 3/31/25.
 //
 
-import Foundation
+import CoreData
+
+final class CoreDataManager {
+    static let shared = CoreDataManager()
+    let container: NSPersistentContainer
+
+    private init() {
+        self.container = NSPersistentContainer(name: "Container")
+        self.container.loadPersistentStores { _, error in
+            if let error = error {
+                fatalError("Failed to load CoreData: \(error)")
+            }
+        }
+    }
+}
