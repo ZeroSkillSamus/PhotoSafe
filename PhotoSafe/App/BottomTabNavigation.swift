6d8463a8c861a1319a9c3f0cac7d3d6ebe3f8600
Abraham Mitchell <abemitch06500@hotmail.com>
2025-04-08 14:42:46 -0400

    Removed navigationstack from album page since it is no longer needed and will be brought to the BottomTabNavigation View

---
+++ PhotoSafe/App/BottomTabNavigation.swift
@@ -0,0 +1,18 @@
+//
+//  TabView.swift
+//  PhotoSafe
+//
+//  Created by Abraham Mitchell on 4/8/25.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabView()
+}
--- PhotoSafe/Features/Album/AlbumView.swift
+++ PhotoSafe/Features/Album/AlbumView.swift
@@ -9,54 +9,50 @@ import SwiftUI
 struct AlbumView: View {
     @EnvironmentObject private var album_VM: AlbumViewModel
     @State private var display_alert: Bool = false
-    @State private var path = NavigationPath()  // For NavigationStack
+    
     @State private var password: String = ""
   
+    @Binding var path: NavigationPath
     var body: some View {
-        NavigationStack(path: $path) {
-            VStack(spacing: 0) {
-                TopHeader(album_vm: self.album_VM)
-                
-                ScrollView {
-                    LazyVStack(spacing: 0) {
-                        ForEach(self.album_VM.albums, id:\.self) { album in
-                            if album.is_locked {
-                                Button {
-                                    self.display_alert = true
-                                } label: {
-                                    AlbumVDisplay(album: album)
-                                }
-                                .alert("Enter Password For \(album.name)",
-                                       isPresented: self.$display_alert)
-                                {
-                                    TextField("Enter Your Password", text: self.$password)
-                                        .foregroundStyle(.white)
-                                        .autocorrectionDisabled()
-                                        .textInputAutocapitalization(.never)
-                                    
-                                    Button("OK", role: .cancel) {
-                                        if let album_password = album.password,
-                                            album_password == password {
-                                            path.append(album)
-                                        }
+        VStack(spacing: 0) {
+            TopHeader(album_vm: self.album_VM)
+            
+            ScrollView {
+                LazyVStack(spacing: 0) {
+                    ForEach(self.album_VM.albums, id:\.self) { album in
+                        if album.is_locked {
+                            Button {
+                                self.display_alert = true
+                            } label: {
+                                AlbumVDisplay(album: album)
+                            }
+                            .alert("Enter Password For \(album.name)",
+                                   isPresented: self.$display_alert)
+                            {
+                                TextField("Enter Your Password", text: self.$password)
+                                    .foregroundStyle(.white)
+                                    .autocorrectionDisabled()
+                                    .textInputAutocapitalization(.never)
+                                
+                                Button("OK", role: .cancel) {
+                                    if let album_password = album.password,
+                                        album_password == password {
+                                        path.append(album)
                                     }
                                 }
-                            } else {
-                                NavigationLink(value: album) {
-                                    AlbumVDisplay(album: album)
-                                }
                             }
-                            Divider()
+                        } else {
+                            NavigationLink(value: album) {
+                                AlbumVDisplay(album: album)
+                            }
                         }
+                        Divider()
                     }
                 }
-               
-                // Ensure Everything Starts At Top of Page
-                Spacer()
             }
-            .navigationDestination(for: AlbumEntity.self) { album in
-                MediaView(album: album)
-            }
+           
+            // Ensure Everything Starts At Top of Page
+            Spacer()
         }
     }
 }
