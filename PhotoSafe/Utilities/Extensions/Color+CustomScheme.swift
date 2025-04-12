61fe41458d3f854f029fb7607f43e83f1bf79aac
Abraham Mitchell <abemitch06500@hotmail.com>
2025-04-12 17:47:02 -0400

    Added new extensions to color 1) Added Hex -> Color conversions 2) Added new schema for app colors

---
+++ PhotoSafe/Utilities/Extensions/Color+CustomScheme.swift
@@ -0,0 +1,30 @@
+//
+//  Color+CustomScheme.swift
+//  PhotoSafe
+//
+//  Created by Abraham Mitchell on 4/12/25.
+//
+
+import SwiftUI
+
+extension Color {
+//    static let c1_text = Color(hex: "f4e7e4")
+//    static let c1_background = Color(hex: "130906")
+//    static let c1_primary = Color(hex: "e1a090")
+//    static let c1_secondary = Color(hex: "8a331d")
+//    static let c1_accent = Color(hex: "e26d4f")
+    
+    
+    
+//    static let c1_text = Color(hex: "e8efec")
+//    static let c1_background = Color(hex: "0f1513")
+//    static let c1_primary = Color(hex: "b1c8c1")
+//    static let c1_secondary = Color(hex: "465864")
+//    static let c1_accent = Color(hex: "8592aa")
+    
+    static let c1_text = Color(hex: "e3e8f0")
+    static let c1_background = Color(hex: "080b0e")
+    static let c1_primary = Color(hex: "a5b6ce")
+    static let c1_secondary = Color(hex: "3e3867")
+    static let c1_accent = Color(hex: "896db0")
+}
